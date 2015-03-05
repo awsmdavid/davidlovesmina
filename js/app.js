dce3da942c40e7016f0507c8e98e067915c032c2
awsmdavid <awsmdavid@gmail.com>
2015-03-05 18:31:29 -0500

    updated rsvp

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ $(document).foundation();
 $('.inactive').hover(function(){
 	$(this).css("border-bottom", "1px solid #888;");
 });
+
+    // alert(CryptoJS.MD5("AOBCD8663"+"2aecc21ce57c973d624175017c3f4616"));
+// rsvp login
+function login() {
+
+    // set hashword (what hash of (password + random hash) equals)
+    var theHashword = "2879a3a3b23a9f5b9ead69529bb5f3c8";
+
+    // enter doubles as submit click
+    $(".content-password").keyup(function(event){
+        if(event.keyCode == 13){
+            $('#submit-password-button').click();
+        }
+    });
+
+    // get user submitted password
+    var password = document.getElementById("password").value;
+
+    // if password is correct
+    if (CryptoJS.MD5(password+"2aecc21ce57c973d624175017c3f4616")==theHashword){
+		// open new window
+        window.open('rsvp'+ CryptoJS.MD5(password) +'.html', 'rsvp!', '_blank');
+    }
+    // if incorrect password
+    else {
+        $('#incorrect-password-message').replaceWith("<span class="+"incorrect-pw-msg panel"+">Incorrect Password,  Please try again!</span>");
+        $('form').get(0).reset();
+    }
+}
