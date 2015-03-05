e666e4cdb4c6c7465b1007ad40e2886f1031de00
awsmdavid <awsmdavid@gmail.com>
2015-03-05 14:00:52 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('.inactive').hover(function(){
+	$(this).css("border-bottom", "1px solid #888;");
+});
